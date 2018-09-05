ec30f22ae1f8598103bdbac034b9ad7e4b845d77
dawsonbooth <github@dawsonbooth.com>
2018-09-05 01:44:29 -0500

    Updated argument parsing

--- ascii_art.py
+++ ascii_art.py
@@ -4,19 +4,8 @@ from PIL import Image
 from image_ops import image_to_ascii
 from weight import weigh_chars
 
-# Parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('image', type=str, help='Path to image from which ASCII art will be generated')
-parser.add_argument('output', type=str, help='Path to generated ASCII art')
-parser.add_argument('-w', '--width', type=int, help='Character width of ASCII art')
-parser.add_argument('-l', '--length', type=int, help='Character length of ASCII art')
-parser.add_argument('-c', '--chars', type=str, help='Path to characters to be seen in ASCII art')
-parser.add_argument('-i', '--invert', type=bool, help='Whether the ASCII output color is inverted')
-
-args = parser.parse_args()
-
 # Main function
-def main():
+def main(args):
 
     # Image
     image = Image.open(args.image)
@@ -51,7 +40,7 @@ def main():
     if args.invert:
         for c in weighted_chars.keys():
             weighted_chars[c] = 1 - weighted_chars[c]
-    
+
     # Generate ASCII art from image
     ascii_art = image_to_ascii(image, width, length, weighted_chars)
 
@@ -64,5 +53,24 @@ def main():
     if width <= 150 and length <= 150:
         print(ascii_art)
 
+
 if __name__ == '__main__':
-    main()
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description='Convert your favorite image into ASCII art')
+    parser.add_argument(
+        'image', type=str, help='Path to image from which ASCII art will be generated')
+    parser.add_argument('output', type=str, help='Path to generated ASCII art')
+    parser.add_argument('-w', '--width', type=int,
+                        help='Character width of ASCII art')
+    parser.add_argument('-l', '--length', type=int,
+                        help='Character length of ASCII art')
+    parser.add_argument('-c', '--chars', type=str,
+                        help='Path to characters to be seen in ASCII art')
+    parser.add_argument('-i', '--invert', type=bool,
+                        help='Whether the ASCII output color is inverted')
+
+    args = parser.parse_args()
+
+    # Run program
+    main(args)
