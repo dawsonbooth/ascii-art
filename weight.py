0883e50318ab54c84a6d202e7ba5518ffd95ce78
dawsonbooth <github@dawsonbooth.com>
2019-03-11 17:41:29 -0500

    Fix invert option

--- weight.py
+++ weight.py
@@ -1,30 +1,27 @@
 from PIL import Image, ImageDraw, ImageFont
 
-# Find number of dark pixels in character
+
 def weigh(c):
-	w, h = (6, 11)
-	font = ImageFont.load_default()
-	fw, fh = font.getsize(c)
-	im = Image.new('L', (fw, fh))
-	draw = ImageDraw.Draw(im)
-	
-	draw.text(((w - fw)/2, (h - fh)/2), c, 255, font=font)
-	im.save('out.png')
-	n = len([p for p in im.getdata() if p>5])
-	return 1 - n / (w * h)
+    """Find and return number of dark pixels in the given character"""
+    w, h = (6, 11)
+    font = ImageFont.load_default()
+    fw, fh = font.getsize(c)
+    im = Image.new('L', (fw, fh))
+    draw = ImageDraw.Draw(im)
 
-# Create dictionary of chars and their weights
-def weigh_chars(chars):
-	weighted_chars = dict()
-	max_weight = 0
-	for c in chars:
-		w =  weigh(c)
-		if w > max_weight:
-			max_weight = w
-		weighted_chars[c] = w
+    draw.text(((w - fw)/2, (h - fh)/2), c, 255, font=font)
+    im.save('out.png')
+    n = len([p for p in im.getdata() if p > 5])
+    return n / (w * h)
 
-	for c in weighted_chars.keys():
-		weighted_chars[c] *= (1 / max_weight)
 
-	return weighted_chars
+def weigh_chars(chars, invert: bool):
+    weighted_chars = dict()
+    for c in chars:
+        w = weigh(c)
+        weighted_chars[c] = w if invert else 1 - w
 
+    for c in weighted_chars.keys():
+        weighted_chars[c] *= (1 / max(weighted_chars.values()))
+
+    return weighted_chars
