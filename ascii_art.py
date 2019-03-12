8589be778ff7e5cbcf6f72d6abaa19262e7ceff0
dawsonbooth <github@dawsonbooth.com>
2019-03-11 19:16:46 -0500

    Modify argument parsing

--- ascii_art.py
+++ ascii_art.py
@@ -9,34 +9,35 @@ def main(args):
 
     image = Image.open(args.image)
 
-    output = args.output
-    if output and not output.endswith('.txt'):
-        output += '.txt'
-
-    width, length = image.size
-    if args.width and args.length:
+    width, height = image.size
+    if args.width and args.height:
         width = args.width
-        length = args.length
-    elif args.width and not args.length:
-        length = int(length * (args.width / width))
+        height = args.height
+    elif args.width and not args.height:
+        height = int(height * (args.width / width))
         width = args.width
-    elif args.length and not args.width:
-        width = int(width * (args.length / length))
-        length = args.length
+    elif args.height and not args.width:
+        width = int(width * (args.height / height))
+        height = args.height
 
-    chars = args.chars if args.chars else ' .\',:;+*?%S#@'
+    if args.chars:
+        with open(args.chars, 'r') as f:
+            chars = f.read().replace('\n', '')
+    else:
+        chars = ' .\',:;+*?%S#@'
     weighted_chars = weigh_chars(chars, args.invert, args.normalize)
 
     # Generate ASCII art from image
-    ascii_art = image_to_ascii(image, width, length, weighted_chars)
+    ascii_art = image_to_ascii(image, width, height, weighted_chars)
 
     # Output ASCII art
-    with open(output, 'wb') as f:
-        f.write(ascii_art.encode('utf-8'))
-    print('\nASCII art saved to ' + output, end='\n\n')
+    if args.output:
+        with open(args.output, 'wb') as f:
+            f.write(ascii_art.encode('utf-8'))
+        print('\nASCII art saved to ' + args.output, end='\n\n')
 
     # Print to console if small enough
-    if width <= 150 and length <= 150:
+    if width <= 150 and height <= 150:
         print(ascii_art)
 
 
@@ -46,16 +47,16 @@ if __name__ == '__main__':
         description='Convert your favorite image into ASCII art')
     parser.add_argument('image', type=str,
                         help='Path to image from which ASCII art will be generated')
-    parser.add_argument('output', type=str, help='Path to generated ASCII art')
+    parser.add_argument('-o', '--output', type=str, help='Path to generated ASCII art')
     parser.add_argument('--width', type=int,
                         help='Character width of ASCII art')
     parser.add_argument('--height', type=int,
                         help='Character height of ASCII art')
     parser.add_argument('--chars', type=str,
                         help='Path to characters to be seen in ASCII art')
-    parser.add_argument('-i', '--invert', type=bool, default=False,
+    parser.add_argument('--invert', action='store_true',
                         help='Whether the ASCII output color is inverted')
-    parser.add_argument('-n', '--normalize', type=bool, default=True,
+    parser.add_argument('-n', '--normalize', action='store_true',
                         help='Whether the weights of the provided ASCII characters are normalized')
     args = parser.parse_args()
 
