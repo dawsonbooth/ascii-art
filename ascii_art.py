2d50fe216187068ca414a3aa59b05d40590496c0
dawsonbooth <github@dawsonbooth.com>
2019-09-04 15:59:06 -0500

    Add terminal argument

--- ascii_art.py
+++ ascii_art.py
@@ -6,7 +6,7 @@ from weight import weigh_chars
 
 
 def main(args):
-    image = Image.open(args.image)
+    image = Image.open(args.input)
 
     width, height = image.size
     if args.width and args.height:
@@ -29,25 +29,25 @@ def main(args):
     # Generate ASCII art from image
     ascii_art = image_to_ascii(image, width, height, weighted_chars)
 
+    # Print to terminal
+    if args.terminal:
+        print(ascii_art)
+
     # Output ASCII art
     if args.output:
         with open(args.output, 'wb') as f:
             f.write(ascii_art.encode('utf-8'))
         print('\nASCII art saved to ' + args.output, end='\n\n')
 
-    # Print to console if small enough
-    if width <= 150 and height <= 150:
-        print(ascii_art)
-
 
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser(
         description='Convert your favorite image into ASCII art')
-    parser.add_argument('image', type=str,
+    parser.add_argument('input', type=str,
                         help='Path to image from which ASCII art will be generated')
-    parser.add_argument('-o', '--output', type=str,
-                        help='Path to generated ASCII art')
+    parser.add_argument('--output', type=str, default=None,
+                        help='Path to output generated ASCII art')
     parser.add_argument('--width', type=int,
                         help='Character width of ASCII art')
     parser.add_argument('--height', type=int,
@@ -56,8 +56,10 @@ if __name__ == '__main__':
                         help='Path to characters to be seen in ASCII art')
     parser.add_argument('--invert', action='store_true',
                         help='Whether the ASCII output color is inverted')
-    parser.add_argument('-n', '--normalize', action='store_true',
+    parser.add_argument('--normalize', action='store_true',
                         help='Whether the weights of the provided ASCII characters are normalized')
+    parser.add_argument('--terminal', action='store_true',
+                        help='Whether to output to the terminal')
     args = parser.parse_args()
 
     # Run program
