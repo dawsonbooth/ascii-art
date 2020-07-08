0518b0a6b38b66188028b8b21117936fa7595329
Dawson Booth <github@dawsonbooth.com>
2020-07-08 17:57:26 -0500

    Update console script

--- console.py
+++ console.py
@@ -1,14 +1,23 @@
 import argparse
 
 
-def main():
+def main(args) -> None:
+    try:
+        print(args.pos)
+        print(f"The first flag is {args.flag1}")
+        if args.verbose:
+            print("Some more info")
+    except KeyboardInterrupt:
+        print("Terminated.")
 
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='')
     parser.add_argument('pos', type=str,
                         help='The positional arg')
     parser.add_argument('-f', '--flag1', type=int, default=0,
-                        help='First page of scraping')
+                        help='This is the first flag')
     parser.add_argument('-p', '--progress', type=bool, default=True,
                         help='Display progress')
     parser.add_argument('-v', '--verbose', type=bool, default=False,
@@ -16,11 +25,4 @@ def main():
 
     args = parser.parse_args()
 
-    try:
-        print()
-    except KeyboardInterrupt:
-        print("Terminated.")
-
-
-if __name__ == '__main__':
-    main()
+    main(args)
