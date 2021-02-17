967008ffdc527433b12dcdc90924f389c973b639
dawsonbooth <github@dawsonbooth.com>
2021-02-16 23:43:44 -0600

    Refactor tasks

--- tasks.py
+++ tasks.py
@@ -1,62 +1,99 @@
 import os
+import platform
 import shutil
+from pathlib import Path
 
-from invoke import task
+from invoke import call, task
+from invoke.context import Context
+from invoke.runners import Result
+
+ROOT = Path(__file__).parent
 
 
-def copy(from_path: str, to_path: str):
-    if os.path.isdir(from_path):
-        shutil.copytree(from_path, to_path)
-    elif os.path.isfile(from_path):
-        shutil.copy(from_path, to_path)
+def _run(c: Context, command: str) -> Result:
+    return c.run(command, pty=platform.system() != "Windows")
 
 
 @task
+def clean_build(c):
+    """Clean up build"""
+    shutil.rmtree(ROOT / "dist", ignore_errors=True)
+
+
+@task
+def clean_docs(c):
+    """Clean up files from documentation builds"""
+    shutil.rmtree(ROOT / "site", ignore_errors=True)
+
+
+@task
+def clean_python(c):
+    """Clean up python file artifacts"""
+    _run(c, f"pyclean {ROOT}")
+
+
+@task
+def clean_tests(c):
+    """Clean up files from testing"""
+    shutil.rmtree(ROOT / ".pytest_cache", ignore_errors=True)
+
+
+@task
+def clean_type_checking(c):
+    """Clean up files from type-checking"""
+    shutil.rmtree(ROOT / ".mypy_cache", ignore_errors=True)
+
+
+@task(pre=[clean_build, clean_docs, clean_python, clean_tests, clean_type_checking])
 def clean(c):
-    c.run("pyclean .")
+    """Run all clean sub-tasks"""
 
 
-@task
-def format(c):
-    c.run("isort MODULE_NAME")
-    c.run("black MODULE_NAME")
-
-
-@task
-def lint(c):
-    c.run("flake8 MODULE_NAME --max-line-length 119 --extend-ignore 'E203, W503'")
+@task(name="format", help={"check": "Checks if source is formatted without applying changes"})
+def format_(c, check=False):
+    """Format code"""
+    isort_options = ["--check-only", "--diff"] if check else []
+    _run(c, f"isort {ROOT} {' '.join(isort_options)}")
+    black_options = ["--diff", "--check"] if check else ["--quiet"]
+    _run(c, f"black {ROOT} {' '.join(black_options)}")
 
 
 @task
 def type_check(c):
-    c.run("mypy MODULE_NAME --ignore-missing-imports")
+    """Run type-checking"""
+    _run(c, f"mypy {ROOT} --ignore-missing-imports")
+
+
+@task(pre=[call(format_, check=True), type_check])
+def lint(c):
+    """Run all linting"""
+    _run(c, f"flake8 {ROOT} --max-line-length 119 --extend-ignore 'E203, W503'")
 
 
 @task
 def test(c):
-    c.run("pytest MODULE_NAME/test")
+    """Run tests"""
+    _run(c, f"pytest {ROOT / 'MODULE_NAME' / 'test'}")
 
 
-@task
-def docs(c):
-    os.makedirs("docs", exist_ok=True)
-    c.run("pydoc-markdown -p MODULE_NAME > docs/api.md")
-    copy("README.md", "docs/")
-    c.run("mkdocs build --clean")
-
-
-@task
-def build(c):
-    c.run("poetry build")
+@task(help={"serve": "Build the docs and watch for changes", "deploy": "Deploy docs to GitHub pages"})
+def docs(c, serve=False, deploy=False):
+    """Build documentation"""
+    os.makedirs(ROOT / "docs", exist_ok=True)
+    _run(c, f"pydoc-markdown -p {ROOT / 'MODULE_NAME'} > {ROOT / 'docs' / 'api.md'}")
+    shutil.copy(ROOT / "README.md", ROOT / "docs")
+    _run(c, "mkdocs build --clean")
+    if deploy:
+        _run(c, "mkdocs gh-deploy")
+    if serve:
+        _run(c, "mkdocs serve")
 
 
 @task
 def publish(c):
-    c.run("mkdocs gh-deploy")
-    c.run("poetry publish")
+    """Publish package to PyPI"""
+    version = _run(c, "poetry version -s").stdout.rstrip()
 
-    version = c.run("poetry version -s").stdout
-
-    c.run(f'git commit -m "v{version}"')
-    c.run(f"git tag v{version}")
-    c.run(f"git push origin v{version}")
+    _run(c, f'git commit -m "v{version}"')
+    _run(c, f"git tag v{version}")
+    _run(c, f"git push origin v{version}")
