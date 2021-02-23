13eff94851d95635aedd7af6cd2d5ef08119e1ec
dawsonbooth <github@dawsonbooth.com>
2021-02-22 21:06:12 -0600

    Fix formatting

--- ascii_art/__init__.py
+++ ascii_art/__init__.py
@@ -5,9 +5,9 @@ def weigh(c, font):
     """Find and return density of dark pixels in the given character"""
     w, h = (6, 11)
     fw, fh = font.getsize(c)
-    im = Image.new('L', (fw, fh))
+    im = Image.new("L", (fw, fh))
     draw = ImageDraw.Draw(im)
-    draw.text(((w - fw)/2, (h - fh)/2), c, 255, font=font)
+    draw.text(((w - fw) / 2, (h - fh) / 2), c, 255, font=font)
     n = len([p for p in im.getdata() if p > 0])
     return n / (w * h)
 
@@ -32,7 +32,7 @@ def find_char(pixel, weighted_chars):
     return min(weighted_chars.keys(), key=lambda c: abs(pixel - weighted_chars[c]))
 
 
-def image_to_ascii(image, width=None, height=None, chars=' .\',:;+*?%S#@', font=None, invert=False, normalize=False):
+def image_to_ascii(image, width=None, height=None, chars=" .',:;+*?%S#@", font=None, invert=False, normalize=False):
     """Convert image to ASCII chars"""
     img_w, img_h = image.size
     if width and not height:
@@ -42,7 +42,7 @@ def image_to_ascii(image, width=None, height=None, chars=' .\',:;+*?%S#@', font=
     elif not (height or width):
         width, height = img_w, img_h
 
-    image = image.resize((width, height)).convert('L')
+    image = image.resize((width, height)).convert("L")
 
     if not font:
         font = ImageFont.load_default()
@@ -52,12 +52,12 @@ def image_to_ascii(image, width=None, height=None, chars=' .\',:;+*?%S#@', font=
     weighted_chars = weigh_chars(chars, font, invert, normalize)
 
     pixels = image.load()
-    ascii_art = ''
+    ascii_art = ""
 
     for i in range(height):
         for j in range(width):
             ascii_art += find_char(pixels[j, i] / 255, weighted_chars)
             if j == width - 1:
-                ascii_art += '\n'
+                ascii_art += "\n"
 
     return ascii_art
--- ascii_art/__main__.py
+++ ascii_art/__main__.py
@@ -7,30 +7,25 @@ from . import image_to_ascii
 
 def main() -> int:
     """ASCII art generator with multiple customization parameters"""
-    parser = argparse.ArgumentParser(
-        description='Convert your favorite image into ASCII art')
-    parser.add_argument('input', type=str,
-                        help='Path to image from which ASCII art will be generated')
-    parser.add_argument('--width', type=int,
-                        help='Character width of ASCII art')
-    parser.add_argument('--height', type=int,
-                        help='Character height of ASCII art')
-    parser.add_argument('--chars', type=str, default=' .\',:;+*?%S#@',
-                        help='String containing characters to be seen in ASCII art')
-    parser.add_argument('--font', type=str,
-                        help='Font for calculating the character weights')
-    parser.add_argument('--invert', action='store_true',
-                        help='Whether the ASCII output color is inverted')
-    parser.add_argument('--normalize', action='store_true',
-                        help='Whether the weights of the provided ASCII characters are normalized')
+    parser = argparse.ArgumentParser(description="Convert your favorite image into ASCII art")
+    parser.add_argument("input", type=str, help="Path to image from which ASCII art will be generated")
+    parser.add_argument("--width", type=int, help="Character width of ASCII art")
+    parser.add_argument("--height", type=int, help="Character height of ASCII art")
+    parser.add_argument(
+        "--chars", type=str, default=" .',:;+*?%S#@", help="String containing characters to be seen in ASCII art"
+    )
+    parser.add_argument("--font", type=str, help="Font for calculating the character weights")
+    parser.add_argument("--invert", action="store_true", help="Whether the ASCII output color is inverted")
+    parser.add_argument(
+        "--normalize", action="store_true", help="Whether the weights of the provided ASCII characters are normalized"
+    )
 
     args = parser.parse_args()
 
     image = Image.open(args.input)
 
     # Generate ASCII art from image
-    ascii_art = image_to_ascii(
-        image, args.width, args.height, args.chars, args.font, args.invert, args.normalize)
+    ascii_art = image_to_ascii(image, args.width, args.height, args.chars, args.font, args.invert, args.normalize)
 
     # Output to stdout
     print(ascii_art)
@@ -38,5 +33,5 @@ def main() -> int:
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(main())
